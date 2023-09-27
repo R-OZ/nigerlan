da828994d21c4ae9c7ee1b8a969dabd9255a1c14
r-oz <reginaldojeba@gmail.com>
2023-09-27 15:07:55 +0100

    updated depencies & edited svg code

--- src/App.js
+++ src/App.js
@@ -2,51 +2,27 @@ import Main from "./components/Main";
 
 import Lecture from "./components/Lecture/Lecture";
 import GrammarLec from "./components/Lecture/Grammar/GrammarLec";
-import Loading from "./components/Loading/Loading";
 
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Feedback from "./components/Profile/Feedback";
-import Test from "./components/Test";
 
 
 const App = () => {
 
   return (
-    <HashRouter>
-      <Switch>
-          <Route exact path="/">
-              <Main />
-          </Route>
-          <Route path="/Alphabetslec">
-              <Lecture id="alphabet" />
-          </Route>
-          <Route path="/Vowelslec">
-              <Lecture id="vowel" />
-          </Route>
-          <Route path="/Consonantslec">
-              <Lecture id="consonant" />
-          </Route>
-          <Route path="/Numberslec">
-              <Lecture id="number" />
-          </Route>
-          <Route path="/Bodylec">
-              <Lecture id="body" />
-          </Route>
-          <Route path="/Grammarlec" >
-              <GrammarLec  />
-          </Route>
-          <Route path="/Feedback">
-              <Feedback />
-          </Route>
-          <Route path="/Loading"> {/*This Component is not yet in use*/}
-              <Loading/>
-          </Route>
-          <Route path="/Test"> {/*This Component is not yet in use*/}
-              <Test />
-          </Route>
-      </Switch>
-    </HashRouter>
+    <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Main />} />
+          <Route path="/Alphabetslec" element={<Lecture id="alphabet" />} />
+          <Route path="/Vowelslec" element= {<Lecture id="vowel" />} />
+          <Route path="/Consonantslec" element={<Lecture id="consonant" />} />
+          <Route path="/Numberslec" element={<Lecture id="number" />} />
+          <Route path="/Bodylec" element={<Lecture id="body" />} />
+          <Route path="/Grammarlec"  element={<GrammarLec  />} />
+          <Route path="/Feedback"  element={<Feedback />} />
+        </Routes>
+    </BrowserRouter>
   )
 }
 export default App;
